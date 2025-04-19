ae616ba0d09a415098dff41038d619ddf93bc429
Janani Senrayaperumal <28778901+jananisenrayaperumal@users.noreply.github.com>
2025-04-19 17:40:33 -0400

    Update persistent.js

--- persistent.js
+++ persistent.js
@@ -140,3 +140,140 @@ promise 1
 ----------
 useCallback vs useMemo
 write a code for both 
+
+
+----------
+
+const arr = [1,1,2,2,3,4,5,5];
+----------
+{
+1: 2
+2: 2
+3: 1
+4:1
+5: 2
+
+}
+
+const removeDuplicate = (arr) => {
+let obj = {};
+
+for(let i=0; i<arr.length; i++){
+  if(obj[arr[i]]){
+    obj[arr[i]] = obj[arr[i]] + 1;
+  } else {
+    obj[arr[i]] = 1;
+  }
+}
+
+return obj
+  
+}
+
+
+console.log(removeDuplicate(arr));
+----------
+
+let start = Date.now();
+ 
+console.log('- - - - script started', start)
+ 
+const getArr1 = () =>{
+  const arr1 = [
+  { name: "Boris", age: 22 },
+  { name: "Peter", age: 23 },
+  { name: "Ivan", age: 27 },
+  { name: "Anna", age: 5 },
+  { name: "Tom", age: 42 },
+  { name: "Amy", age: 23 },
+  { name: "Anna", age: 22 },
+  { name: "Boris", age: 19 }];
+ 
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('arr1', Date.now() - start, 'ms')
+      resolve(arr1);
+    }, 500)
+  })
+}
+ 
+const getArr2 = () => {
+  var arr2 = [
+  { name: "Boris", age: 40 },
+  { name: "Peter", age: 23 },
+  { name: "David", age: 27 },
+  { name: "Boris", age: 3 },
+  { name: "Anna", age: 31 },
+  { name: "Boris", age: 47 },
+  { name: "Amy", age: 35 },
+  { name: "Sam", age: 22 },
+  { name: "Boris", age: 31 }];
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+      console.log('arr2 runtime took', Date.now() - start, 'ms')
+     resolve(arr2);
+      }, 800)
+    })
+}
+ 
+ 
+const solution =  async () => {
+let arr1 = await getArr1();
+let arr2 = await getArr2();
+ 
+return [...arr1, ...arr2];
+}
+
+solution()
+
+----------
+React
+API call and button click and show data
+=> https://jsonplaceholder.typicode.com/users
+
+app.js
+import React, { useState } from 'react';
+import './style.css';
+import Button from './Button.js';
+
+export default function App() {
+  const [users, setUsers] = useState();
+
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+      <Button setUsers={setUsers} />
+      <div>
+        {users &&
+          users.map((user, i) => {
+            return <div key={user.id}>{user.name}</div>;
+          })}
+      </div>
+    </div>
+  );
+}
+
+//btn comp, btn in app.js
+
+button.js
+import React from 'react';
+
+function Button({setUsers}) {
+  const handleClick = async () => {
+    const res = fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res;
+    const users = await data.json();
+    setUsers(users);
+    console.log(users);
+  };
+
+  return (
+    <div>
+      <button onClick={() => handleClick()}>Button</button>
+    </div>
+  );
+}
+
+export default Button;
+----------
